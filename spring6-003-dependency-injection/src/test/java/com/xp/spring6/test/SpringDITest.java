4bdc5ef4f70fa26adc7b4b9e3def2b795d5df08d
xupan <3355429520@qq.com>
2023-07-20 11:54:39 +0800

    spring version1.7

--- spring6-003-dependency-injection/src/test/java/com/xp/spring6/test/SpringDITest.java
+++ spring6-003-dependency-injection/src/test/java/com/xp/spring6/test/SpringDITest.java
@@ -99,5 +99,6 @@ public class SpringDITest {
         System.out.println("test 4 ......");
         System.out.println("test 5 ......");
         System.out.println("test 6 ......");
+        System.out.println("test 7 ......");
     }
 }
